9276cf99ae10a132506228668308b51040a78bd6
LidetuWendyifraw <lidetu190@gmail.com>
2026-07-06 20:16:04 +0300

    classimage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -63,7 +63,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16 lg:h-20">
           <button onClick={() => navigate('/')} className="flex items-center gap-2 group">
             <div className="w-10 h-10 rounded-lg bg-primary-600 flex items-center justify-center group-hover:scale-110 transition-transform">
-             <img src="src\components\images\logo.jpg" alt="the gym-logo" />
+             <img src="src\components\images\logo.jpg" alt="the gym-logo" className="w-10 h-10 rounded-full object-cover group-hover:scale-110 transition-transform"/>
             </div>
             <span className="font-display text-xl font-bold text-white tracking-wider">
               POWER PLUS &nbsp;<span className="text-primary-500">GYM</span>
