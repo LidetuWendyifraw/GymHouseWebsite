d3bfddd524a1850cb86d4eab12094ffc9e4a2cb6
LidetuWendyifraw <lidetu190@gmail.com>
2026-07-06 19:48:50 +0300

    updating the logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -63,7 +63,7 @@ export default function Navbar() {
         <div className="flex items-center justify-between h-16 lg:h-20">
           <button onClick={() => navigate('/')} className="flex items-center gap-2 group">
             <div className="w-10 h-10 rounded-lg bg-primary-600 flex items-center justify-center group-hover:scale-110 transition-transform">
-             <img src="C:\Users\User\OneDrive\Desktop\project\src\types\logo.jpg" alt="the gym-logo" />
+             <img src="logo.jpg" alt="the gym-logo" />
             </div>
             <span className="font-display text-xl font-bold text-white tracking-wider">
               POWER PLUS<span className="text-primary-500">GYM</span>
